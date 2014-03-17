5fa3e2e138674304331816591b910b15c6cbee09
Juniorchen <Juniorchen2010@163.com>
2014-03-17 11:20:27 +0800

    pragma mark for decorate functions

---
+++  UITableViewDelegate / Table view delegate / / IDECodeSnippetCompletionPrefix: tableviewdelegate / IDECodeSnippetCompletionScopes: [ClassImplementation] / IDECodeSnippetIdentifier: 8ABC912C-BC69-43D5-94E4-83E585E671A1 / IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C / IDECodeSnippetUserSnippet: 1 /PragmaMark.m
@@ -0,0 +1,10 @@
+// Pragma Mark
+// pragma mark
+//
+// IDECodeSnippetCompletionPrefix: mark
+// IDECodeSnippetCompletionScopes: [ClassImplementation]
+// IDECodeSnippetIdentifier: 8ABC912C-BC69-43D5-94E4-83E585E671A1
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+#pragma mark - #name#
