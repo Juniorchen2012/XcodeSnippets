42815e91961ab6b92a98e3fd104d4f61799ecf05
Juniorchen <Juniorchen2010@163.com>
2014-05-18 13:46:41 +0800

    no message

--- todo.m
+++ todo.m
@@ -1,10 +1,11 @@
-// TODO: 
-// TODO: 
+// TODO:
+// TODO:
 //
 // IDECodeSnippetCompletionPrefix: todo
 // IDECodeSnippetCompletionScopes: [ClassImplementation]
 // IDECodeSnippetIdentifier: A55799AC-17C8-4D47-9BCF-6D4F6643B950
-// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetLanguage:
+// Xcode.SourceCodeLanguage.Objective-C/Users/mitbbs/Library/Developer/Xcode/UserData/CodeSnippets/A55799AC-17C8-4D47-9BCF-6D4F6643B950.m
 // IDECodeSnippetUserSnippet: 1
 // IDECodeSnippetVersion: 2
-#//TODO: 
+// TODO:
